94097b965c8d031724fc85f5768d854cf42396fa
balsipson <thirdi@gmail.com>
2023-03-23 17:21:30 +0530

    write to file test

--- index.js
+++ index.js
@@ -283,6 +283,27 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.post('/', (req, res) => {
+  let data = '';
+  req.on('data', chunk => {
+    data += chunk.toString();
+  });
+
+  req.on('end', () => {
+    const text = data.split('=')[1];
+    const filePath = '/tmp/myFile.txt';
+    fs.writeFile(filePath, text, err => {
+      if (err) {
+        console.error(err);
+        res.sendStatus(500);
+      } else {
+        console.log('File written successfully');
+        res.sendStatus(200);
+      }
+    });
+  });
+});
+
 app.get('/webhook', (req, res) => {
 	let mode = req.query["hub.mode"];
 	let token = req.query["hub.verify_token"];
